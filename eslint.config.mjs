4a4790c1db729ad18d23699297632ea0b3d57685
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-05-29 16:11:39 +0300

    CS: multiline-ternary, yoda, no-unmodified-loop-condition, no-useless-return, no-control-regex

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,14 +19,9 @@ export default [
       'one-var': 'off',
       '@stylistic/key-spacing': 'off',
       'prefer-const': 'off',
-      '@stylistic/multiline-ternary': 'off',
       'no-useless-escape': 'off',
       '@stylistic/no-mixed-operators': 'off',
-      yoda: 'off',
       'object-shorthand': 'off',
-      'no-control-regex': 'off',
-      'no-unmodified-loop-condition': 'off',
-      'no-useless-return': 'off'
     }
   }
 ]
