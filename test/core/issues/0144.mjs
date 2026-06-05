8d54939b8df248502972725202f0825e14819b76
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-06-06 01:28:52 +0300

    Drop #144 test - not actual after rewrite

--- test/core/issues/0144.mjs
+++
@@ -1,8 +0,0 @@
-import { it } from 'node:test'
-
-import assert from 'node:assert'
-import { load } from 'js-yaml'
-
-it('Infinite loop when attempting to parse multi-line scalar document that is not indented', () => {
-  assert.throws(() => load('--- |\nfoo\n'), /missing indentation for block scalar/)
-})
