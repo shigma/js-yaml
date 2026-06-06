ffb2513539d3a129a5af0ba379aa408fec482119
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-06-06 03:22:40 +0300

    Skip incorrect #280 test

--- test/core/issues/0280.mjs
+++ test/core/issues/0280.mjs
@@ -3,7 +3,10 @@ import { it } from 'node:test'
 import assert from 'node:assert'
 import { load } from 'js-yaml'
 
-it('Loader must throw an error on zero-indentation block scalar', () => {
+it.skip('Loader must throw an error on zero-indentation block scalar', () => {
+  // See https://github.com/nodeca/js-yaml/issues/280
+  // This conflists with W4TN, M7A3, DWX9 of yaml 1.2 test suite.
+
   assert.throws(() => load('--- |\nfoo\n'), /missing indentation for block scalar/)
   assert.throws(() => load('|-\nfoo\nbar'), /missing indentation for block scalar/)
   assert.throws(() => load('>\nfoo\nbar'), /missing indentation for block scalar/)
