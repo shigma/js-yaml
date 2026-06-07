eef69381ee1d87f7bd314d80ae205eddeb019238
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-06-07 07:31:56 +0300

    test: move some fixtures to tag tests

---
+++ test/core/tags/custom.test.mjs
@@ -0,0 +1,116 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert/strict'
+import util from 'node:util'
+import { load, CORE_SCHEMA, defineMappingTag, defineScalarTag } from 'js-yaml'
+
+function Tag1 (parameters) {
+  this.x = parameters.x
+  this.y = parameters.y || 0
+  this.z = parameters.z || 0
+}
+
+function Tag2 () {
+  Tag1.apply(this, arguments)
+}
+util.inherits(Tag2, Tag1)
+
+function Tag3 () {
+  Tag2.apply(this, arguments)
+}
+util.inherits(Tag3, Tag2)
+
+function Foo (parameters) {
+  this.myParameter = parameters.myParameter
+  this.myAnotherParameter = parameters.myAnotherParameter
+}
+
+const schema = CORE_SCHEMA.withTags([
+  defineMappingTag('!tag3', {
+    create: () => new Tag3({}),
+    addPair: (container, key, value) => {
+      if (key === '=' || key === 'x') container.x = value
+      else if (key === 'y') container.y = value
+      else if (key === 'z') container.z = value
+    },
+    has: () => false,
+    keys: (container) => Object.keys(container),
+    get: (container, key) => container[key],
+    identify: (object) => object instanceof Tag3
+  }),
+
+  defineScalarTag('!tag2', {
+    resolve: (source) => new Tag2({ x: parseInt(source, 10) }),
+    identify: (object) => object instanceof Tag2
+  }),
+
+  defineMappingTag('!tag1', {
+    create: () => new Tag1({}),
+    addPair: (container, key, value) => {
+      if (key === 'x') container.x = value
+      else if (key === 'y') container.y = value
+      else if (key === 'z') container.z = value
+    },
+    has: () => false,
+    keys: (container) => Object.keys(container),
+    get: (container, key) => container[key],
+    identify: (object) => object instanceof Tag1
+  }),
+
+  defineMappingTag('!foo', {
+    create: () => new Foo({}),
+    addPair: (container, key, value) => {
+      if (key === 'my-parameter') container.myParameter = value
+      else if (key === 'my-another-parameter') container.myAnotherParameter = value
+    },
+    has: () => false,
+    keys: (container) => Object.keys(container),
+    get: (container, key) => container[key],
+    identify: (object) => object instanceof Foo
+  })
+])
+
+describe('tags', () => {
+  it('custom', () => {
+    const src = `
+- !tag1
+  x: 1
+- !tag1
+  x: 1
+  'y': 2
+  z: 3
+- !tag2
+  10
+- !tag3
+  x: 1
+- !tag3
+  x: 1
+  'y': 2
+  z: 3
+- !tag3
+  =: 1
+  'y': 2
+  z: 3
+- !foo
+  my-parameter: foo
+  my-another-parameter: [1,2,3]
+`
+    const expected = [
+      new Tag1({ x: 1 }),
+      new Tag1({ x: 1, y: 2, z: 3 }),
+      new Tag2({ x: 10 }),
+      new Tag3({ x: 1 }),
+      new Tag3({ x: 1, y: 2, z: 3 }),
+      new Tag3({ x: 1, y: 2, z: 3 }),
+      new Foo({ myParameter: 'foo', myAnotherParameter: [1, 2, 3] })
+    ]
+
+    const actual = load(src, { schema })
+
+    assert.equal(actual.length, expected.length)
+
+    for (let i = 0; i < expected.length; i++) {
+      assert.deepStrictEqual(actual[i], expected[i])
+      assert.strictEqual(Object.getPrototypeOf(actual[i]), Object.getPrototypeOf(expected[i]))
+    }
+  })
+})
--- test/core/tags/float.test.mjs
+++ test/core/tags/float.test.mjs
@@ -2,23 +2,34 @@ import { describe, it } from 'node:test'
 import assert from 'node:assert/strict'
 import { load } from 'js-yaml'
 
-describe('tags', () => {
-  it('float', () => {
+describe('tags/float', () => {
+  it('common', () => {
     const src = `
 - 6.8523015e+5  # canonical
 - 685.23015e+03 # exponentical
 - 685230.15     # fixed
 - -.inf
 - .NaN
+
+- . # single dot is not a float
+- -1.0
+- +1.0
+- +.inf
+- .nan
 `
     const expected = [
-      685230.15,
-      685230.15,
-      685230.15,
-      Number.NEGATIVE_INFINITY,
-      NaN
-    ]
+      685230.15, 685230.15, 685230.15, Number.NEGATIVE_INFINITY, NaN,
 
+      '.', -1.0, 1.0, Number.POSITIVE_INFINITY, NaN
+    ]
     assert.deepStrictEqual(load(src), expected)
+
+    const specials = `
+normalZero: 0.0
+negativeZero: -0.0
+`
+    const actual = load(specials)
+    assert.strictEqual(Object.is(actual.normalZero, 0.0), true)
+    assert.strictEqual(Object.is(actual.negativeZero, -0.0), true)
   })
 })
