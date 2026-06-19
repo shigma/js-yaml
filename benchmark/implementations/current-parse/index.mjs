bc57eb9be8c8a900106cbb41bec237b5f5d68e4a
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-06-19 12:54:21 +0300

    Fix benchmark to new api

--- benchmark/implementations/current-parse/index.mjs
+++ benchmark/implementations/current-parse/index.mjs
@@ -1,6 +1,5 @@
-import { parseEvents, createParserState } from '../../../src/parser/parser.ts'
+import { parseEvents } from '../../../src/parser/parser.ts'
 
 export function run (data) {
-  const state = createParserState(data)
-  return parseEvents(state)
+  return parseEvents(data, {})
 }
