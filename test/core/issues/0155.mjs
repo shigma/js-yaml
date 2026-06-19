4a3d76300f935be91b8432ffde22590a393f80c8
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-06-20 00:03:33 +0300

    Remove outdated issue tests (coverage not changed)

--- test/core/issues/0155.mjs
+++
@@ -1,8 +0,0 @@
-import { it } from 'node:test'
-
-import assert from 'node:assert'
-import { load } from 'js-yaml'
-
-it('Named null', () => {
-  assert.deepStrictEqual(load('---\ntest: !!null \nfoo: bar'), { test: null, foo: 'bar' })
-})
--- test/core/issues/0160.mjs
+++
@@ -1,8 +0,0 @@
-import { it } from 'node:test'
-
-import assert from 'node:assert'
-import { load } from 'js-yaml'
-
-it('Correct encoding of UTF-16 surrogate pairs', () => {
-  assert.strictEqual(load('"\\U0001F431"'), '🐱')
-})
