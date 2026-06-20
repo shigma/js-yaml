6be5d46c88f4caed3103e83075c2eb4aab8770fd
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-06-20 23:53:55 +0300

    tests: drop not actual or duplicating issue tests (covered in other places)

--- test/core/issues/0117.mjs
+++
@@ -1,8 +0,0 @@
-import { it } from 'node:test'
-
-import assert from 'node:assert'
-import { dump } from 'js-yaml'
-
-it('Negative zero loses the sign after dump', () => {
-  assert.strictEqual(dump(-0), '-0.0\n')
-})
